8de9372d74c98ed8c9529556866acdf18964e39f
Shailesh Kumar <shailesh.kumar@outlook.com>
2018-04-04 00:06:19 +0530

    NavigationSetup: Initial Commit

---
+++ NavigationSetup/src/index.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('NavigationSetup', () => App);
+AppRegistry.runApplication('NavigationSetup', {
+  rootTag: document.getElementById('root')
+});
